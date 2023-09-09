8a3bc71dc28aad215637fcc1106efc7aacc59e70
abhyudaya29 <abhyudayadev123@gmail.com>
2023-09-09 17:05:23 +0530

    React Components

--- app.js
+++ app.js
@@ -5,13 +5,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement('h1',{id:"h1"},"Oye hoye💀☕❤️🚀")
-const Root=ReactDOM.createRoot(document.getElementById('root'))
+// const heading=React.createElement('h1',{id:"h1"},"Oye hoye💀☕❤️🚀")
+// const Root=ReactDOM.createRoot(document.getElementById('root'))
 
-console.log(heading)
-const jsxHeading=<h1 className="balle">Oye balle balle☕🚀🚀🚀🚀</h1>
-console.log(jsxHeading  )
-Root.render(jsxHeading)
+// // console.log(heading)
+// const jsxHeading=(<h1 className="balle">Oye balle 
+//                 balle☕🚀🚀🚀🚀</h1>)
+// console.log(jsxHeading )
+// Root.render(jsxHeading)
 // Root.render(heading);
 
 // js=> React.create element=>React element i.e a js object=>Html content after renderd
+
+// React components
+//1) Class based components
+//2) functional based components 
+const ele=<span>Hattt</span>
+const Veer=()=> (
+    <h1 className="H">Veer oye oye</h1>)
+
+// Root.render(<Veer/>)
+
+const number=10;
+const Tara=()=>{
+    return(  
+        <div>
+            <Veer/>
+            {ele}
+            {console.log("Dubbey kai bubbey")}
+            <h1 id="H">Tarra pajji</h1>
+        </div>
+    )
+}
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Tara />)
+// root.render(<Veer/>)
